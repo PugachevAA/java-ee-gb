e28a81f0e3fa69c692ed23be4fc5cf2ac2bacffd
Alexey <pugachev.aa@mail.ru>
2022-01-08 21:17:51 +0700

    code from lesson 2

---
+++ src/main/java/ru/gb/lesson2/hello/WantMessageProvider.java
@@ -0,0 +1,37 @@
+package ru.gb.lesson2.hello;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+//@Primary
+@Profile("want")
+@Scope("prototype")
+public class WantMessageProvider implements MessageProvider {
+
+    @InjectFoodStuff
+    String foodStuff;
+
+    public WantMessageProvider() {
+        System.out.println("foodstuff from constructor: " + foodStuff);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("foodstuff from init: " + foodStuff);
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println("PREDESTROY");
+    }
+    @Override
+    public String getMessage() {
+        return "I want " + foodStuff;
+    }
+}
